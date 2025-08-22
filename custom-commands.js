560513b036179b518e806f55c2a5c23df60cca23
ZeissPresets <storezeiss@gmail.com>
2025-08-23 00:42:15 +0700

    Delete custom-commands.js

--- custom-commands.js
+++
@@ -1,7 +0,0 @@
-{
-  "hi": "Halo! Ada yang bisa saya bantu?",
-  "hello": "Hello! How can I help you?",
-  "terimakasih": "Sama-sama! Senang bisa membantu :)",
-  "thanks": "You're welcome! Glad to help :)",
-  "bot": "Saya adalah bot WhatsApp yang dibuat menggunakan Node.js dan library Baileys"
-}
